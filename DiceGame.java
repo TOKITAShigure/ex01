d891073583c9749e9d2f0b098772bc81d4ecd62d
TOKITAShigure <136042325+TOKITAShigure@users.noreply.github.com>
2023-06-09 14:28:56 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -19,7 +19,7 @@ class DiceGame{
 	System.out.println("Die 1: "+dice1);
 	System.out.println("Die 2: "+dice2);
 	System.out.println("Total value: "+total);
-	if(total>=7) System.out.println("You won");
-	else System.out.println("You lost");
+	if(total>=7) System.out.println(name+" won!");
+	else System.out.println(name+" lost!");
     }
 }
